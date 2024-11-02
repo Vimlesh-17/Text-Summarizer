4ee42249187354c6db56ffd88ef893e628c61231
Vimlesh-57 <vimleshc7317@gmail.com>
2024-11-03 02:35:03 +0530

    data validation added

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_01_pipeline import DataIngestionTrainingPipeline
+from src.TextSummarizer.pipeline.stage_02_pipeline import DataValidationTrainingPipeline
 
 # logger.info("Welcome to the cult")
 STAGE_NAME = "Data Ingestion Step"
+
 
 try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Step"
+
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f">>> stage {STAGE_NAME} completed <<<")
 except Exception as e:
     logger.exception(e)
