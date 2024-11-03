1ca1ee9a8e86012fd2ea46e51c31061c4745aa89
Vimlesh-57 <vimleshc7317@gmail.com>
2024-11-03 15:48:19 +0530

    evaluation done

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from src.TextSummarizer.logging import logger
 from src.TextSummarizer.pipeline.stage_01_pipeline import DataIngestionTrainingPipeline
 from src.TextSummarizer.pipeline.stage_02_pipeline import DataValidationTrainingPipeline
 from src.TextSummarizer.pipeline.stage_03_pipeline import DataTransformationTrainingPipeline
+from src.TextSummarizer.pipeline.stage_04_pipeline import ModelTrainerTrainingPipeline
+
+from src.TextSummarizer.pipeline.stage_05_pipeline import ModelEvaluationTrainingPipeline
+
 
 # logger.info("Welcome to the cult")
 STAGE_NAME = "Data Ingestion Step"
@@ -36,6 +40,31 @@ try:
     logger.info(f">>> stage {STAGE_NAME} started <<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Model Trainer Step"
+
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation Step"
+
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    model_evaluator = ModelEvaluationTrainingPipeline()
+    model_evaluator.main()
     logger.info(f">>> stage {STAGE_NAME} completed <<<")
 except Exception as e:
     logger.exception(e)
